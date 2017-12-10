6a4df885cdff9238d2bd4d272faf03a7865accbd
vrojo <vin.rojo@gmail.com>
2017-12-10 19:45:42 +0100

    Checking sup + leger changement visuel

--- Website/site/js/index.js
+++ Website/site/js/index.js
@@ -17,7 +17,7 @@ window.onload = function(){
     }
 
     function performPostRequest(choice) {
-        var form = document.getElementById('formula').value
+        form = document.getElementById('formula').value
         axios.post('http://localhost:8081/', {
             formula: form
         })
@@ -25,12 +25,17 @@ window.onload = function(){
             console.log(response);
             if (choice == 0){
                 paragraph(0);
+                checkingMore(str);
+                res = document.getElementById('postResult');
+                res.style.visibility = "hidden";
             } else{
-                //TODO le calcul
                 str = document.getElementById('formula');
-                answer = calculate(parseCalculationString(str.value));
                 paragraph(0);
-                result(answer);
+                errCheck = checkingMore(str);
+                if (errCheck == 0){
+                    answer = calculate(parseCalculationString(str.value));
+                    result(answer);
+                }
             }
         })
         .catch(function (error) {
@@ -39,30 +44,59 @@ window.onload = function(){
         });
     }
 
+    function checkingMore(str){
+        str.value = str.value.toString().replace(/\s+/g, '');
+        partStr = ['/+', '/-', '/*', '//',
+                        '*+', '*-', '**', '*/',
+                         '++', '+-', '+*', '+/',
+                         '-+', '--', '-*', '-/'];
+        for (var word in partStr){
+            if(str.value.toString().indexOf(partStr[word] + "") !== -1){
+                paragraph(-10);
+                return -1;;
+            }
+        }
+        if (str.toString().substring(0, 1) == "*" || str.toString().substring(0, 1) == "/" || str.toString().substring(0, 1) == "+" || str.toString().substring(0, 1) == "-" || str.toString().substring(0, 1) == "=") {
+            paragraph(-10);
+            return -1;
+        } else if (str.toString().indexOf("*", str.length - 1) !== -1 || str.toString().indexOf("/", str.length - 1) !== -1 || str.toString().indexOf("+", str.length - 1) !== -1 || str.toString().indexOf("-", str.length - 1) !== -1 || str.toString().indexOf("=", str.length - 1) !== -1) {
+            paragraph(-10);
+            return -1;
+        } else if(str.value.toString().indexOf('/0') !== -1){
+            paragraph(-2);
+            return -1;
+        }
+        return 0;
+    }
+
     function paragraph(err){
         p = document.getElementById('msg');
+        res = document.getElementById('postResult');
         switch(err) {
           case 0:
             p.innerHTML = 'ok!'
             p.style.color = "green";
             break;
-          case 1:
-            p.innerHTML = ' '
-            p.style.color = "green";
-            break;
           case -1:
             p.style.color = "red";
             p.innerHTML = 'Error, enter a formula...';
+            res.style.visibility = "hidden";
             break;
+          case -2:
+            p.style.color = "red";
+            p.innerHTML = 'MathError...'
+            res.style.visibility = "hidden";
+              break;
           default:
             p.style.color = "red";
             p.innerHTML = 'Error, formula is wrongly written...';
+            res.style.visibility = "hidden";
         }
     }
 
     function result(val){
       p = document.getElementById('postResult');
-      p.innerHTML = 'Result:'+val;
-      p.style.color = "green";
+      p.innerHTML = 'Result : '+val;
+      p.style.visibility = "visible";
     }
 }
